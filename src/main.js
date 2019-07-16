66e745b3a2d15efea62bf928145000ae3bea3b46
daihe <1834470384@qq.com>
2019-07-16 17:14:41 +0800

    1714

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-console.log('main.js')
+import '../static/css/commen.css'
+import {b} from './watcher/watcher'
+
+let a = [1, 2, 3]
+console.log([...a], b)
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const CleanWebpackPlugin = require('clean-webpack-plugin');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const srcDir = path.join(__dirname, './src');
 const distDir = path.join(__dirname, './dist');
 
 module.exports = {
     mode: "development",
-    entry: path.join(__dirname, './src/main.js'),
+    entry: {
+        index: [
+            path.join(__dirname, './src/main.js')
+        ],
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/[name].min.js'
@@ -37,12 +41,10 @@ module.exports = {
             {
                 test: /\.css$/,
                 exclude: /node_modules/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: {
-                        loader: 'css-loader'
-                    },
-                })
+                use: [
+                    "style-loader",
+                    "css-loader",
+                ],
             },
             {
                 test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
@@ -54,14 +56,21 @@ module.exports = {
     //     new CleanWebpackPlugin(['dist']),
 
     //   new ExtractTextPlugin('style/[name].min.css'),
-    //   new HtmlWebpackPlugin({
-    //       hash: true,
-    //       chunks: ['index'],
-    //       template: "./src/pages/index/index.html",
-    //       filename: "pages/index/index.html"
-    //   }),
+        new HtmlWebpackPlugin({
+            hash: true,
+            chunks: ['index'],
+            template: "./index.html",
+            filename: "index.html"
+        }),
       
     //   new webpack.HotModuleReplacementPlugin(),
     ],
-
+    devServer: {
+        clientLogLevel: 'warning',
+        historyApiFallback: true,
+        hot: true,
+        compress: true,
+        host: 'localhost',
+        port: 8081
+    }
 }
