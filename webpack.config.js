0c6736614a06542222345ed7655bfe862a9909fa
daihe <1834470384@qq.com>
2019-07-16 15:47:41 +0800

    1547

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const srcDir = path.join(__dirname, './src');
+const distDir = path.join(__dirname, './dist');
+
+module.exports = {
+    mode: "development",
+    entry: path.join(__dirname, './src/main.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].min.js'
+
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: {
+                        loader: 'css-loader'
+                    },
+                })
+            },
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                loader: 'url-loader?limit=8192&name=img/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+    //     new CleanWebpackPlugin(['dist']),
+
+    //   new ExtractTextPlugin('style/[name].min.css'),
+    //   new HtmlWebpackPlugin({
+    //       hash: true,
+    //       chunks: ['index'],
+    //       template: "./src/pages/index/index.html",
+    //       filename: "pages/index/index.html"
+    //   }),
+      
+    //   new webpack.HotModuleReplacementPlugin(),
+    ],
+
+}
