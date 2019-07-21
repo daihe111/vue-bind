d6255e1bafcc4364645cd95d9d8de9176ded380c
daihe <1834470384@qq.com>
2019-07-21 10:58:35 +0800

    1058

--- src/main.js
+++ src/main.js
@@ -15,3 +15,25 @@ document.getElementById('data-changer').onclick = (e) => {
     vm.data.text = vm.data.text + 'vue文本';
 };
 
+function domDFS (node) {
+    const res = [],
+    stack = [];
+    let currentNode = null;
+    stack.push(node);
+    while (stack.length) {
+        currentNode = stack.pop();
+        res.push(currentNode);
+
+        const children = currentNode.children;
+        for (let i = children.length; i >=0 ; i--) {
+            const child = children[i];
+            if (child) {
+                stack.push(child);
+            }
+        }
+    }
+    return res;
+}
+
+console.log(domDFS(document.getElementsByTagName('body')[0]));
+
